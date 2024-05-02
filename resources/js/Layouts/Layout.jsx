3c18b310c368b0beb536b53f52bba037e9d9f0c3
saad-mage4 <saad@mage4.com>
2024-05-02 17:26:50 +0500

    login changes

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -4,7 +4,7 @@ import Navbar from "@/Components/Navbar";
 const Layout = ({ children, user }) => {
     return (
         <>
-            <Navbar userID={user?.id} />
+            <Navbar userID={user?.id} userName={user?.name} />
             {children}
             <Footer/>
         </>
