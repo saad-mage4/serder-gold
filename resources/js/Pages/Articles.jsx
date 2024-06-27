1cd8ec2b59d0e95576d771b85632fcc8f2693731
saad-mage4 <saad@mage4.com>
2024-06-27 19:59:49 +0500

    update

--- resources/js/Pages/Articles.jsx
+++ resources/js/Pages/Articles.jsx
@@ -1,4 +1,5 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 const Articles = ({ auth }) => {
     return (
         <>
@@ -9,7 +10,29 @@ const Articles = ({ auth }) => {
                         Articles
                     </h2>
                 }
-            ></AuthenticatedLayout>
+            >
+                <Head title="Articles Page | " />
+
+                <div className="py-12">
+                    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div className="p-6 text-gray-900">
+                                <section className="update-images">
+                                    <header>
+                                        <h2 className="text-lg font-medium text-gray-900">
+                                            List Articles
+                                        </h2>
+
+                                        <p className="mt-1 text-sm text-gray-600">
+                                            list of all articles.
+                                        </p>
+                                    </header>
+                                </section>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </AuthenticatedLayout>
         </>
     );
 };
--- resources/js/Pages/Users.jsx
+++ resources/js/Pages/Users.jsx
@@ -1,4 +1,5 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 const Users = ({ auth }) => {
     return (
         <>
@@ -9,7 +10,29 @@ const Users = ({ auth }) => {
                         Users
                     </h2>
                 }
-            ></AuthenticatedLayout>
+            >
+                <Head title="Users Page | " />
+
+                <div className="py-12">
+                    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div className="p-6 text-gray-900">
+                                <section className="update-images">
+                                    <header>
+                                        <h2 className="text-lg font-medium text-gray-900">
+                                            List Users
+                                        </h2>
+
+                                        <p className="mt-1 text-sm text-gray-600">
+                                            list of all users.
+                                        </p>
+                                    </header>
+                                </section>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </AuthenticatedLayout>
         </>
     );
 };
