d3ed5e8045ab2be0a65619a6e0f372273b63938a
saad-mage4 <saad@mage4.com>
2024-06-27 20:20:16 +0500

    update

--- resources/js/Pages/Articles.jsx
+++ resources/js/Pages/Articles.jsx
@@ -17,7 +17,7 @@ const Articles = ({ auth }) => {
                     <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                         <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                             <div className="p-6 text-gray-900">
-                                <section className="update-images">
+                                <section className="articles-section">
                                     <header>
                                         <h2 className="text-lg font-medium text-gray-900">
                                             List Articles
