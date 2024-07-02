84976b36c6bb24924680b6449491dfa59261c66f
saad-mage4 <saad@mage4.com>
2024-07-02 16:51:37 +0500

    update

--- resources/js/Pages/addArticles.jsx
+++ resources/js/Pages/addArticles.jsx
@@ -188,7 +188,7 @@ const addArticles = ({ auth }) => {
                                                     type="submit"
                                                     className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                                 >
-                                                    Sign in
+                                                    Add Article
                                                 </button>
                                             </div>
                                         </form>
